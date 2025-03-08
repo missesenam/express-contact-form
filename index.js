26f7c2de610594263e53a5354c7a231cde0feae4
missesenam <moneyesenam@gmail.com>
2025-03-08 15:30:42 +0100

    my commit

--- index.js
+++ index.js
@@ -1,32 +1,63 @@
-const express = require("express");
+const http = require("http");
+const fs = require("fs");
 const path = require("path");
-const bodyParser = require("body-parser");
+const { URLSearchParams } = require("url");
 
-// server
-const server = express();
-
-// middleware def
-server.use(express.static(path.join(__dirname, "public")));
-server.use(bodyParser.urlencoded({ extended: false }));
-
-// route request handlers
-const handleLogin = (req, res) => {
-  //   //   console.log(req);
-  //   let body = "";
-  //   req.on("data", (chunk) => {
-  //     body += chunk;
-  //   });
-  //   req.on("end", () => {
-  //     // parse with regExp
-
-  //     console.log(body);
-  //   });
-  console.log(req.body);
-  console.log(req.body.name);
-  res.send("Done");
+// Function to serve static files
+const serveStaticFile = (filePath, contentType, res) => {
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404, { "Content-Type": "text/plain" });
+      res.end("404 Not Found");
+    } else {
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(data);
+    }
+  });
 };
 
-// route
-server.post("/login", handleLogin);
+// Function to handle form submissions
+const handleLogin = (req, res) => {
+  let body = "";
+  req.on("data", (chunk) => {
+    body += chunk;
+  });
 
-server.listen(3000, () => console.log("server is ready"));
+  req.on("end", () => {
+    const params = new URLSearchParams(body);
+    console.log("Form Data:", Object.fromEntries(params));
+    console.log("Name:", params.get("name"));
+
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Login successful!");
+  });
+};
+
+// HTTP server
+const server = http.createServer((req, res) => {
+  const filePath = path.join(
+    __dirname,
+    "public",
+    req.url === "/" ? "index.html" : req.url
+  );
+
+  if (req.method === "GET") {
+    const ext = path.extname(filePath);
+    let contentType = "text/html";
+
+    if (ext === ".css") contentType = "text/css";
+    else if (ext === ".js") contentType = "application/javascript";
+
+    serveStaticFile(filePath, contentType, res);
+  } else if (req.method === "POST" && req.url === "/login") {
+    handleLogin(req, res);
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("404 Not Found");
+  }
+});
+
+// Start the server
+server.listen(3000, () =>
+  console.log("Server running at http://localhost:3000")
+);
